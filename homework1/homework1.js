737a1ad642b14437e4efb1ec2f79c42772b5a41c
Oleg <oleg2759064@gmail.com>
2021-04-14 13:34:27 +0300

    Fix Homework_1

--- homework1/homework1.js
+++ homework1/homework1.js
@@ -20,23 +20,22 @@ if (!Boolean(a) || a == Infinity || a < 0) {
 
 info_length = parseInt(prompt("Введите количество студентов"))
 
-for (i = 1, info = []; i < info_length + 1; i++) {
+for (i = 1, info = [], sum = 0; i < info_length + 1; i++) {
     info.push(prompt('Введите возраст, имя, фамилию студента №' + i));
-    for (i = 0, sum = 0; i < info.length; i++) {
-        sum += parseInt(info[i].split(" ")[0]);
-        if (!Boolean(info[i].split(" ")[0])) {
-            alert('Некоректные данные');
-        }
+    sum += parseInt(info[i - 1].split(" ")[0]);
+    if (isNaN(info[i])) {
+        alert('Некоректные данные');
+        break;
     }
 }
 
-average = sum / info.length;
+average = sum / info_length;
 
 document.write(star + br); 
 document.write("Список студентов зарегестрировавшихся на вебинар:" + br)
 
 for (i = 0; i < info_length; i++) {
-    document.write((i+1) + ". " +info[i] + br); 
+    document.write((i+1) + ". " + info[i] + br); 
 }
 
 document.write(line + "Средний возраст студента: " + average +line + br);
